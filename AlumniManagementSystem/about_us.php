83b3f90fca30db243c113569da335065254331cc
Chin Hui <chhew96@gmail.com>
2019-10-31 23:16:19 +0800

    Create about_us.php

---
+++ AlumniManagementSystem/about_us.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About Us</title>
+<link rel="stylesheet" href="css/about_us.css" />
+
+<?php
+include_once"setting/aboutus_navigation.php";
+?>
+</head>
+
+<body>
+<div id="history">
+<p class="p1">The History</p>
+<p class="p2"> INTRODUCTION TO THE INSTITUTE OF POSTGRADUATE STUDIES (IPS) </p>
+<p class="p3">The Institute of Postgraduate Studies (IPS) supervises the graduate programs in UMP. This includes administering the admission, appointment of supervisors, candidature reviews, provision of financial assistance and assessment. The centre is headed by a Dean who is assisted by administrative officers and administrative assistants.</p><br />
+<p class="p2">GOVERNANCE OF GRADUATE STUDIES</p>
+<p class="p3">The Dean of IPS leads the administrative processes relating to graduate studies in UMP. He reports to the Vice-Chancellor and is a member of the Senate of UMP. The policy setting for graduate studies are endorsed by the Senate which are recommended and formulated by the Senate’s Permanent Sub-Committee on Graduate Studies. The Sub Committee oversees the admission, appointment of supervisors and the assessment of graduate candidates.  For routine administrative processes, the Dean of IPS is assisted by Graduate Studies Management Committee that is chaired by the dean and comprise of deputy deans whom are responsible for graduate studies in their respective faculties.</p><br />
+<p class="p2">FUNCTION OF IPS</p>
+<p class="p3">The function of IPS includes the following:
+<ul class="ul2">
+	<li>Promoting UMP’s graduate programs</li>
+	<li>Facilitate the appointment of supervisor for graduate student</li>
+	<li>Oversees the provision of financial assistance</li>
+	<li>Coordinate the end of semester appraisal of candidature</li>
+	<li>Organise the mid-term candidature review </li>
+	<li>Coordinate the assessment for graduation purposes</li>
+	<li>Formulate administrative policies pertaining to graduate studies</li>
+	<li>Organise activities to enhance graduate students research capabilities</li>
+	<li>Organise activities to enhance the supervisory abilities of UMP’s academic staff</li>
+</ul>
+</p>
+<br><br><br>
+<br /><hr color="peru" size="4"/>
+</div>
+<br><br><br>
+<div id="organization_chart">
+<p class="p1">Organization chart</p>
+<p class="about_image"><img src="image/organization chart.jpg"/></p>
+<br><br><br><br>
+</div>
+</body>
+</html>
